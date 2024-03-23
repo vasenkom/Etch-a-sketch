92b2b08abbdbeea6ac7ba3fe8eb2147119828c1c
vasenkom <m.vasenko@yandex.ru>
2024-03-23 20:54:02 +0100

    add: blackButton to draw with black color; eraser to fix drawing; randomButton to draw with random colors (works if isRandomColorEnabled = true)

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const canvas = document.getElementById('canvas');
 const setSizeButton = document.getElementById('sizeSetter');
 const resetButton = document.getElementById('reset');
+const blackButton = document.getElementById('black');
+const randomButton = document.getElementById('random');
+const eraser = document.getElementById('eraser');
+
+let isRandomColorEnabled = false; //if true when randomButton works
+let selectedColor = 'black'; // default color
 
 setSizeButton.addEventListener('click', function() {
     const widthHeightCanvas = document.getElementById('widthHeight').value;
     
-    // Clear the existing canvas
+    //Clear the existing canvas
     canvas.innerHTML = "";
 
-    createGrid (widthHeightCanvas);
-    
+    createGrid (widthHeightCanvas); 
 })
 
 //Creating the canvas
@@ -21,6 +26,17 @@ function createGrid (widthHeightCanvas) {
             const square = document.createElement("div");
             square.classList.add("square");
             square.style.border = "1px solid black"
+
+            square.addEventListener('mouseover', function() {
+                if (isRandomColorEnabled) {
+                    //Set random color on hover if randomButton was clicked
+                    square.style.backgroundColor = hexRandom(); 
+                } else {
+                    //Set selected color on hover if randomButton was not clicked
+                    square.style.backgroundColor = selectedColor; 
+                }
+            });
+            
             row.appendChild(square);
         }
         canvas.appendChild(row);
@@ -32,4 +48,32 @@ resetButton.addEventListener('click', function () {
     canvas.innerHTML = "";
 })
 
+//Button to draw with black color only
+blackButton.addEventListener('click', function() {
+    selectedColor = 'black';
+    isRandomColorEnabled = false;
+});
 
+//Button to use eraser (basicly just white color)
+eraser.addEventListener('click', function() {
+    selectedColor = 'white';
+    isRandomColorEnabled = false;
+});
+
+//Button to draw with random colors
+randomButton.addEventListener('click', function() {
+    isRandomColorEnabled = true;
+});
+
+//Adds random color to background
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+function hexRandom () {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomColor()];
+    }
+    return hexColor;
+}
+function getRandomColor() {
+    return Math.floor(Math.random() * hex.length)
+}
